d685e0b511ebf64158996fa1cad64b984c39afe2
Undo1 <parker@erwaysoftware.com>
2014-02-24 07:06:04 -0700

    add script.js

--- index.php
+++ index.php
@@ -15,12 +15,36 @@
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="script.js"></script>
+
   </head>
   <body>
   <div class="text-center" style="color:orange; font-size:90px; margin-top:20px"><span class="glyphicon glyphicon-fire"></span></div>
   <h1 class='text-center'>Blaze</h1>
 
+  <table class="col-md-offset-2">
+
+    <tr><td style="vertical-align:top" class="col-md-1">
+    <div class="score">
+    <h2 style='color:rgba(0,0,0,0.6); pull:right'>12</h2>
+    </div>
+    </td>
+    <td class=''>
+    <div class="post col-md-9">
+      <h3><a>Test link</a></h3>
+      <hr>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ante lacus, vestibulum vitae consequat sit amet, ornare rhoncus nisi. Curabitur congue placerat risus, at vestibulum magna sollicitudin sed. Nunc cursus hendrerit consequat. Maecenas molestie justo sit amet risus rutrum, ut sagittis erat egestas. Quisque volutpat velit non arcu sagittis condimentum. Sed nec varius eros, eget eleifend tellus. Vivamus a neque augue. Mauris eget bibendum nulla, a bibendum est.</p>
+
+      <p>Proin placerat ornare urna. Phasellus sit amet eros ac velit elementum commodo eu sed nibh. Maecenas tempor sit amet lorem ut tempor. Donec molestie velit purus, fringilla dapibus erat mattis in. Morbi leo dui, semper sit amet laoreet ac, bibendum pulvinar libero. Aliquam vel ante eget odio laoreet viverra et a felis. Sed bibendum quam ac dapibus egestas. Proin mattis convallis tempor. Nam mattis nulla quis augue tincidunt, vitae auctor augue sodales. Nulla sapien justo, dapibus quis mauris eu, pulvinar porta mi. Pellentesque fermentum leo nec sagittis varius. Praesent fermentum elit sit amet massa ornare fringilla. Ut justo magna, aliquet sit amet nisl et, imperdiet cursus velit. Pellentesque a gravida ante.</p>
+    </div>
+    </td>
+    </tr>
+
+  </table>
+
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
   </body>
 </html>
