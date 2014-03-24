44af0513a015c30dbed705822c4d4d4500e5129f
Undo1 <parker@erwaysoftware.com>
2014-03-23 18:10:20 -0600

    magic

--- SmokeDetector/report.py
+++ SmokeDetector/report.py
@@ -2,20 +2,9 @@
 import json,os,sys,getpass
 from ChatExchange.SEChatWrapper import *
 
-if("ChatExchangeU" in os.environ):
-  username=os.environ["ChatExchangeU"]
-else:
-  print "Username: "
-  username=raw_input()
-if("ChatExchangeP" in os.environ):
-  password=os.environ["ChatExchangeP"]
-else:
-  password=getpass.getpass("Password: ")
-
-
 #wrap=SEChatWrapper("MSO")
 wrap=SEChatWrapper("SE")
-wrap.login(username,password)
+wrap.login(sys.argv[2],sys.argv[3])
 s="\[[Blaze](https://github.com/Charcoal-SE/Blaze)] %s" % sys.argv[1] 
 print s
 wrap.sendMessage("11540",s)
