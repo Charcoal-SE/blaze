24b5dad800dbf80934003123036f30b4e762a549
Undo1 <parker@erwaysoftware.com>
2014-03-25 17:50:21 -0600

    magic

--- posttochat.php
+++ posttochat.php
@@ -1,4 +1,17 @@
 <?php
-include 'config.php';
+include 'base.php';
 $question_url = $_POST["url"];
+
+$db = PDODatabaseObject();
+$stmt = $db->prepare("SELECT * FROM flags WHERE postUrl=':url'");
+$stmt->execute(array(':url' => $question_url));
+if ($stmt->rowCount() > 0)
+{
+	echo "already flagged";
+	return;
+}
+$stmt = $db->prepare("INSERT INTO flags(postUrl) VALUES(:postUrl)");
+$stmt->execute(array(':postUrl' => $question_url));
+
 echo exec("python SmokeDetector/report.py 'answer flagged: " . escapeshellarg($question_url) . "' '" . SEChatUsername() . "' '" . SEChatPassword() . "'");
+
