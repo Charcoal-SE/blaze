51a0c4afbe9685a760435c9938be1bef4ab2190e
ProgramFOX <programfox@hotmail.be>
2014-08-19 12:53:44 +0200

    Fixed #11

--- posttochat.php
+++ posttochat.php
@@ -8,18 +8,38 @@ if (!$_SESSION['Blaze_LoggedIn'])
 }
 
 
-$question_url = $_POST["url"];
+$answer_id = $_POST["id"];
+$site = $_POST["site"];
 $body = $_POST["body"];
+$link = "http://";
+
+$site_list = array("stackoverflow", "meta.stackoverflow", "meta.stackexchange", "askubuntu", "meta.askubuntu",
+	"mathoverflow", "meta.mathoverflow", "superuser", "meta.superuser", "serverfault", "meta.serverfault");
 
 $db = PDODatabaseObject();
 $stmt = $db->prepare('SELECT COUNT(*) FROM flags WHERE postUrl=:url');
-$stmt->execute(array(":url" => $question_url));
+$stmt->execute(array(":url" => $site . $answer_id));
 if ($stmt->fetchColumn() > 0)
 {
 	echo "already flagged";
 	return;
 }
+if (preg_match("/[0-9]+/", $answer_id) < 1)
+{
+	echo "not a valid id";
+	return;
+}
+if (in_array($site, $site_list))
+{
+	$link = $link . $site . ".com";
+}
+else
+{
+	$link = $link . $site . ".stackexchange.com";
+}
+$link = $link . "/a/" . $answer_id;
 $stmt = $db->prepare("INSERT INTO flags(postUrl, body) VALUES(:postUrl, :body)");
-$stmt->execute(array(':postUrl' => $question_url, ':body' => $body));
+$stmt->execute(array(':postUrl' => $site . $answer_id, ':body' => $body));
 
-echo exec("python report.py 'answer flagged by " . escapeshellarg($_SESSION['Blaze_Username']) . ": " . escapeshellarg($question_url) . "' '" . SEChatUsername() . "' '" . SEChatPassword() . "'");
+echo exec("python report.py 'answer flagged by " . escapeshellarg($_SESSION['Blaze_Username']) . ": " . escapeshellarg($link) . "' '" . SEChatUsername() . "' '" . SEChatPassword() . "'");
+echo "success";
--- script.js
+++ script.js
@@ -104,7 +104,7 @@ $(document).ready(function() {
 					}
 					else if (apiEndpoint == 'answers')
 					{
-						$("table").append(RenderAnswer(item));
+						$("table").append(RenderAnswer(item, site));
 					}
 					else if (apiEndpoint == 'comments')
 					{
@@ -167,22 +167,17 @@ $(document).ready(function() {
 		console.log("flag button pressed");
 		var flagButton = $(this);
 		flagButton.html("<strong>working...</strong>");
-		var postLink = $(this).attr("postLink");
+		var postId = $(this).attr("data-postid");
+		var siteName = $(this).attr("data-site");
 		var body = $(this).siblings("span.post-body").html();
-		console.log(postLink);
-		var argString = "url=" + postLink + "&body=" + body;
+		console.log(postId);
+		var argString = "id=" + postId + "&site=" + siteName + "&body=" + body;
 		$.ajax({
 		    type: "POST",
 		 	url: "/blaze/posttochat.php",
 		 	data: argString,
 		 	success: function(data) {
-		 		console.log("success");
-		 		if (data == "already flagged") {
-		 			flagButton.html(data);
-		 		}
-		 		else {
-		 			flagButton.html("flagged");
-		 		}
+		 		console.log(data);
 			},
 			error: function(data) {
 				console.log("error")
@@ -192,7 +187,7 @@ $(document).ready(function() {
 
 	//Rendering things
 
-	function RenderAnswer(item)
+	function RenderAnswer(item, site)
 	{
 		var string = '<tr><td style="vertical-align:top" class="col-md-1"><div class="score"><h2 style="color:rgba(0,0,0,0.6); pull:right">';
 		string = string + item["score"];
@@ -204,7 +199,7 @@ $(document).ready(function() {
 		string = string + '</h3><hr><span class="post-body" style="color:rgba(70,70,70,1)">';
 		string = string + item["body"];
 		string = string + '</span>'
-		if (isLoggedIn) string = string + '<a class="flag" style="float:left; color:rgb(165,65,65)" href="#" postLink="' + item["link"] + '"><strong>flag</strong></a>';
+		if (isLoggedIn) string = string + '<a class="flag" style="float:left; color:rgb(165,65,65)" href="#" data-site="' + site + '" data-postid="' + item["link"].split("#")[1] + '"><strong>flag</strong></a>';
 		string = string + RenderUsercard(item["owner"], item);
 		string = string + '</p></div></td></tr>';
 		string = string + '<tr><td class="col-md-1"></td></tr>'; //<td><strong style="color:#b65454">flag</strong></td>
