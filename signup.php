1308d51c791946066422fcbe07ecf0779e503819
hichris1234 <christopherncarmel@hotmail.com>
2014-08-22 16:27:12 -0400

    Cleanup!

--- signup.php
+++ signup.php
@@ -7,19 +7,18 @@ $password = md5($_POST["password"]);
 $username = $_POST["username"];
 $email = $_POST["email"];
 
-$stmt = $db->prepare("SELECT * FROM users WHERE username=:username OR email:=email");
-$stmt->execute(array(':username' => $username, ':email' => $email));
-$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$count = $db->prepare("SELECT COUNT(*) FROM users WHERE username=:username OR email:=email");
+$count->execute(array(':username' => $username, ':email' => $email));
+$numofrows = $count->fetchColumn();
 
-if (count($rows) > 0)
-{
-	echo 'username or email already taken.';
-	return;
+if ($rows > 0) {
+    echo 'username or email already taken.';
+    return;
 }
 
-$stmt = $db->prepare("INSERT INTO users(username,email,password) VALUES(:username,:email,:password)");
-$stmt->execute(array(':username' => $username, ':email' => $email, ':password' => $password));
-$affected_rows = $stmt->rowCount();
+$insertuser = $db->prepare("INSERT INTO users VALUES(:username,:email,:password)");
+$insertuser->execute(array(':username' => $username, ':email' => $email, ':password' => $password));
+$affected_rows = $insertuser->rowCount();
 
 if ($affected_rows == 1) echo 'success';
 else echo 'something weird went wrong';
