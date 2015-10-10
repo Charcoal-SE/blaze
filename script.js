22f970761d373dadcdcdeab53c7ea6a551f6359b
ArtOfCode- <owen.jenkins2903@gmail.com>
2015-10-10 11:56:12 +0100

    add email warning check

--- script.js
+++ script.js
@@ -668,7 +668,7 @@ $(document).ready(function() {
 		
 		var checks = {
 			"ContainsTelephone": function(text) {
-				var matches = text.match(/[0-9A-Z\.\-\+]{0, 13}/gi);
+				var matches = text.match(/[0-9A-Z\.\-\+]{0,15}/gi);
 				if(matches) {
 					for(var match in matches) {
 						var testCountries = ["US", "IN"];
@@ -684,6 +684,10 @@ $(document).ready(function() {
 					return false;
 				}
 				else return false;
+			},
+			"ContainsEmail": function(text) {
+				// Thanks voyager (http://stackoverflow.com/users/34813) (http://stackoverflow.com/a/1373724/3160466)
+				return text.match(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/gi);
 			},
 			"PostLengthUnderThreshold": function(text) {
 				return text.length < 100;
